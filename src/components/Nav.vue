c7908ccbef76bee55f1ec161603814a25c185590
pikesley <sam.pikesley@gmail.com>
2018-05-12 23:03:12 +0100

    Bend it to our purposes

---
+++ src/components/Nav.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id='nav'>
+    <a
+      class='toggle small-disc'
+      v-on:click='show = !show'
+      >
+      <icon name='bars'></icon>
+    </a>
+    <transition name='fade'>
+      <ul
+        class='nav flex-column'
+        id='nav'
+        v-if='show'
+        >
+        <li>
+          <router-link
+            v-bind:to="'/'"
+            >
+            Question
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            v-bind:to="'/about'"
+            >
+            About
+          </router-link>
+        </li>
+      </ul>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      show: false
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,39 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
 
-Vue.config.productionTip = false
+import App from './App'
+import Question from './components/Question'
+import About from './components/About'
+
+Vue.use(Buefy, { defaultIconPack: 'fa' })
+Vue.use(VueRouter)
+
+Vue.component('icon', Icon)
+
+const routes = [
+  {
+    path: '/',
+    component: Question
+  },
+  {
+    path: '/about',
+    component: About
+  }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   components: { App },
-  template: '<App/>'
-})
+  template: '<App/>',
+  router
+}).$mount('#app')
