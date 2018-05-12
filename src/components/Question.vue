3d186fe3b326e0505a5e50aef85c42d100dc83e3
pikesley <sam.pikesley@gmail.com>
2018-05-13 00:02:11 +0100

    FIx ugly console warnings

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -43,7 +43,10 @@ export default {
   name: 'Question',
   data () {
     return {
-      question: null
+      question: {
+        premise: '',
+        options: []
+      }
     }
   },
   mounted () {
